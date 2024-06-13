a8dfe002d6519df89cb45a7fef754184dcdcf1e6
aherrera125 <j.alejandroherrera@gmail.com>
2024-06-13 13:28:21 +0200

    Add validations

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-function submit(){
-    console.log("Entra al js");
-    let fName = document.getElementById("fname");
-    let lName = document.getElementById("lname");
-    let eMail = document.getElementById("email");
-    let tMessage = document.getElementById("message");
-    console.log(eMail);
+function sendData() {
+    let fName = document.getElementById("fname").value;
+    let lName = document.getElementById("lname").value;
+    let eMail = document.getElementById("email").value;
+    let gender = document.getElementById("gender").value;
+    let tMessage = document.getElementById("message").value;
 
-    if(eMail.contains('@')){
-        alert("email valid");        
-    }else{
+    if (eMail.includes("@")) {
+        if (eMail.length > 3 && eMail.length < 20) {
+            if (fName && lName) {
+                alert("Email valid. Name: " + fName + " " + lName + ". Gender: " + gender + ". Message: " + tMessage);
+                clear();
+            } else {
+                alert("Complete data required");
+            }
+        } else {
+            alert("Name longer than 3 letters, and less than 20 letters");
+        }
+    } else {
         alert("Email no valid");
     }
+}
+
+function clear() {
+    document.getElementById("fname").value = "";
+    document.getElementById("lname").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("gender").value = "Select";
+    document.getElementById("message").value = "";
 }
